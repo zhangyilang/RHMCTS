b89daa49912b9b4f24f49dac0267b5f20ea58f7b
XieBinghui <33569201+XieBinghui@users.noreply.github.com>
2019-06-15 10:59:55 +0800

    add the evaluation policy

--- policy.py
+++ policy.py
@@ -1,4 +1,6 @@
 from random import choice, sample
+from utils import *
+from copy import deepcopy
 
 
 heuristic4_p1 = {(1, 1, 1, 1, 0): 4, (1, 1, 1, 0, 1): 3, (1, 1, 0, 1, 1): 2, (1, 0, 1, 1, 1): 1, (0, 1, 1, 1, 1): 0}
@@ -11,25 +13,95 @@ h3 = {1: heuristic3_p1, 2: heuristic3_p2}
 
 
 # 辉哥这两个函数交给你了
+# 好的哥
 def policy_evaluation_function(state):
-    '''
+    """
     Return a list of the best n substates (n might change for different states) for current player given current state;
     :param state: [board, player], where board is a 2-d list and player is either 1 or 2;
     :return: a list of the best n substates. Each substate is a tuple ((x, y), prob) of a board coordinate to place the
              piece and corresponding estimated winning prob for current player.
-    '''
+    """
     board, player = state
-    pass
+    moved = [] # the coordinates placed by chess piece
+    for i in range(20):
+        for j in range(20):
+            if board[i][j] > 0:
+                moved.append((i, j))
+
+    substate = []
+    adjacent = adjacent_moves(moved)  # get the adjacent of the moved
+
+    # suppose the coordinates in the adjacent have been placed by chess piece
+    moved = moved + adjacent
+    new_adjacent = adjacent_moves(moved)  # the adjacent' adjacent
+    adjacent = adjacent + new_adjacent
+
+    sum = 0  # normalization factor
+
+    for (x, y) in adjacent:
+        board_new = deepcopy(board)
+        if board_new[x][y] > 0:
+            continue
+        else:
+            board_new[x][y] = player
+            score = board_evaluation(board_new)
+            sum += score
+            substate.append(((x, y), score))
+
+    # Normalize the sub-state
+    sub = []
+    for item in substate:
+        coord = item[0]
+        score_ori = item[1]
+        score_new = score_ori / sum
+        sub.append((coord, score_new))
+
+    # choose the 5 sub-state with the highest scores
+    sub = sorted(sub, key=lambda x: x[1])[:5]
+
+    return tuple(sub)
 
 
 def simulation_evaluation_function(state):
-    '''
+    """
     Simplified version of policy_evaluation function. Return more possible actions without probability.
     :param state: [board, player]
     :return: [(x0, y0), (x1, y1), ... , (xn, yn)]
-    '''
+    """
     board, player = state
-    pass
+    moved = []  # the coordinates placed by chess piece
+    for i in range(20):
+        for j in range(20):
+            if board[i][j] > 0:
+                moved.append((i, j))
+
+    substate = []
+    adjacent = adjacent_moves(moved)  # get the adjacent of the moved
+
+    sum = 0  # normalization factor
+
+    for (x, y) in adjacent:
+        board_new = deepcopy(board)
+        if board_new[x][y] > 0:
+            continue
+        else:
+            board_new[x][y] = player
+            score = board_evaluation(board_new)
+            sum += score
+            substate.append(((x, y), score))
+
+    # Normalize the sub-state
+    sub = []
+    for item in substate:
+        coord = item[0]
+        score_ori = item[1]
+        score_new = score_ori / sum
+        sub.append((coord, score_new))
+
+    # choose the 5 sub-state with the highest scores
+    sub = sorted(sub, key=lambda x: x[1])[:15]
+
+    return tuple(sub)
 
 
 def coarse_policy_eva_fn(state):
---
+++ utils.py
@@ -0,0 +1,213 @@
+import re
+from collections import Counter
+
+
+def extend_board(player, board):
+    """
+    add an edge for the board
+    since the ege play the same role as the opponent for the player
+    we set the ege as the opponent ( 3 - player )
+    """
+    new_board = [[board[x-1][y-1] if 0 < x < 20 + 2 and 0 < y < 20 + 2 else 3-player \
+                  for x in range(20 + 2)] for y in range(20 + 2)]
+    return new_board
+
+
+def is_special_class(board, player):
+    """
+    judge whether the several chess given in the list form a special class
+    :param
+        board: the board of gomoku
+        player: the index of color, 1: black, 2: white
+    :return:
+        Counter: ({class: num of this class}, ...)
+    """
+
+    # Details in 'http://zjh776.iteye.com/blog/1979748'
+
+    def _black_color(board):
+        height, width = len(board), len(board[0])
+        for i in range(height):
+            for j in range(width):
+                board[i][j] = (3 - board[i][j]) % 3
+        return board
+
+    if player == 2:
+        list_str = _black_color(board)
+
+    class_dict = {("WIN", (), ()): "11111",
+                  ("H4", (0, 5), ()): "011110",
+                  ("C4", (0), (5)): "011112",
+                  ("C4", (5), (0)): "211110",
+                  ("C4", (4), ()): r"^11110",
+                  ("C4", (0), ()): r"01111$",
+                  ("C4", (0, 2, 6), ()): "0101110",
+                  ("C4", (0, 4, 6), ()): "0111010",
+                  ("C4", (0, 3, 6), ()): "0110110",
+                  ("H3", (0, 4), ()): "01110",
+                  ("H3", (0, 2, 5), ()): "010110",
+                  ("H3", (0, 3, 5), ()): "011010",
+                  ("M3", (0, 1), (5)): "001112",
+                  ("M3", (0, 1), ()): r"00111$",
+                  ("M3", (4, 5), (0)): "211100",
+                  ("M3", (4, 5), ()): r"^11100",
+                  ("M3", (0, 2), (5)): "010112",
+                  ("M3", (0, 2), ()): r"01011$",
+                  ("M3", (3, 5), (0)): "211010",
+                  ("M3", (3, 5), ()): r"^11010",
+                  ("M3", (0, 3), (5)): "011012",
+                  ("M3", (0, 3), ()): r"01101$",
+                  ("M3", (2, 5), (0)): "210110",
+                  ("M3", (2, 5), ()): r"^10110",
+                  ("M3", (1, 2), ()): "10011",
+                  ("M3", (2, 3), ()): "11001",
+                  ("M3", (1, 3), ()): "10101",
+                  ("M3", (1, 4), (0, 6)): "2011102",
+                  ("M3", (1, 4), (6)): r"^011102",
+                  ("M3", (1, 4), (0)): r"201110$",
+                  ("H2", (0, 1, 4), ()): "00110",
+                  ("H2", (0, 3, 4), ()): "01100",
+                  ("H2", (0, 2, 4), ()): "01010",
+                  ("H2", (0, 2, 3, 5), ()): "010010",
+                  ("M2", (0, 1, 2), (5)): "000112",
+                  ("M2", (0, 1, 2), ()): r"00011$",
+                  ("M2", (3, 4, 5), (0)): "211000",
+                  ("M2", (3, 4, 5), ()): r"^11000",
+                  ("M2", (0, 1, 3), (5)): "001012",
+                  ("M2", (0, 1, 3), ()): r"00101$",
+                  ("M2", (2, 4, 5), (0)): "210100",
+                  ("M2", (2, 4, 5), ()): r"^10100",
+                  ("M2", (0, 2, 3), (5)): "010012",
+                  ("M2", (0, 2, 3), ()): r"01001$",
+                  ("M2", (2, 3, 5), (0)): "210010",
+                  ("M2", (2, 3, 5), ()): r"^10010",
+                  ("M2", (1, 2, 3), ()): "10001",
+                  ("M2", (1, 3, 5), (0, 6)): "2010102",
+                  ("M2", (1, 3, 5), (0)): r"201010$",
+                  ("M2", (1, 3, 5), (6)): r"^010102",
+                  ("M2", (1, 4, 5), (0, 6)): "2011002",
+                  ("M2", (1, 4, 5), (6)): r"^011002",
+                  ("M2", (1, 4, 5), (0)): r"201100^",
+                  ("M2", (1, 2, 5), (0, 6)): "2001102",
+                  ("M2", (1, 2, 5), (0)): r"200110$",
+                  ("M2", (1, 2, 5), (6)): r"^001102",
+                  ("S4", (), (0, 5)): "211112",
+                  ("S4", (), (0)): r"21111$",
+                  ("S4", (), (5)): r"^11112",
+                  ("S3", (), (0, 4)): "21112",
+                  ("S3", (), (0)): r"2111$",
+                  ("S3", (), (4)): r"^1112",
+                  ("S2", (), (0, 3)): "2112",
+                  ("S2", (), (3)): r"^112",
+                  ("S2", (), (0)): r"211$",
+                  }
+
+    height, width = len(board), len(board[0])
+    class_counter = Counter()
+
+    # scan by row
+    for row_idx, row in enumerate(board):
+        list_str = "".join(map(str, row))
+        for key in class_dict:
+            class_counter[key[0]] += len(re.findall(class_dict[key], list_str))
+
+    # scan by col
+    for col_idx in range(width):
+        col = [a[col_idx] for a in board]
+        list_str = "".join(map(str, col))
+        for key in class_dict:
+            class_counter[key[0]] += len(re.findall(class_dict[key], list_str))
+
+    # scan by diag_1, from TL to BR
+    for dist in range(-width + 1, height):
+        row_ini, col_ini = (0, -dist) if dist < 0 else (dist, 0)
+        diag = [board[i][j] for i in range(
+            row_ini, height) for j in range(col_ini, width) if i - j == dist]
+        list_str = "".join(map(str, diag))
+        for key in class_dict:
+            class_counter[key[0]] += len(re.findall(class_dict[key], list_str))
+
+    # scan by diag_2, from BL to TR
+    for dist in range(0, width + height - 1):
+        row_ini, col_ini = (dist, 0) if dist < height else (
+            height - 1, dist - height + 1)
+        diag = [board[i][j] for i in range(
+            row_ini, -1, -1) for j in range(col_ini, width) if i + j == dist]
+        list_str = "".join(map(str, diag))
+        for key in class_dict:
+            class_counter[key[0]] += len(re.findall(class_dict[key], list_str))
+
+    return class_counter
+
+
+def class_to_score():
+    """
+    define the reward of some specific class of chess
+    :return:
+        score_map: a map from the special class(a string) to score(a real number)
+    """
+    score_map = {"WIN": 100000,
+                 "H4": 10000,
+                 "C4": 100,
+                 "H3": 200,
+                 "M3": 50,
+                 "H2": 5,
+                 "M2": 3,
+                 "S4": -5,
+                 "S3": -5,
+                 "S2": -5
+                 }
+    return score_map
+
+
+def board_evaluation(board):
+    """
+    evaluate the situation of the brain.
+    :param
+        board:
+    :return:
+        score: a real number, indicating how good the condition is
+    """
+    score = 0
+
+    brain_board = extend_board(board=board, player=1)
+    for a_class, num in is_special_class(brain_board, 1).items():
+        score = score + class_to_score()[a_class] * num
+
+    oppo_board = extend_board(board=board, player=2)
+    for a_class, num in is_special_class(oppo_board, 2).items():
+        score = score - class_to_score()[a_class] * num
+
+    return score
+
+
+def adjacent_moves(moved):
+    """
+    find the neighbors of the moved
+    """
+    adjacent = set()
+    width = 20
+    height = 20
+
+    for (h, w) in moved:
+        if h < width - 1:
+            adjacent.add((h+1, w))  # right
+        if h > 0:
+            adjacent.add((h-1, w))  # left
+        if w < height - 1:
+            adjacent.add((h, w+1))  # upper
+        if w > 0:
+            adjacent.add((h, w-1))  # lower
+        if w < width - 1 and h < height - 1:
+            adjacent.add((h+1, w+1))  # upper right
+        if h > 0 and w < height - 1:
+            adjacent.add((h-1, w+1))  # upper left
+        if h < width - 1 and w > 0:
+            adjacent.add((h+1, w-1))  # lower right
+        if w > 0 and h > 0:
+            adjacent.add((h-1, w-1))  # lower left
+
+    adjacent = list(set(adjacent) - set(moved))
+
+    return adjacent
+
